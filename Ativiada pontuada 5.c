92f0126a86565919b1ccf6dbc66eda27f3fb0f2d
Maicon Lima <143419560+Maicon17Lima@users.noreply.github.com>
2023-11-25 16:09:04 -0300

    Add files via upload

---
+++ Ativiada pontuada 5.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    int opcao = 0;
+    char nome[9999];
+    float preco[9999];
+    int quantidade[9999];
+    int contador = 0;
+    int i,j,k;
+    float total =0;
+    int vendas;
+
+    while (opcao != 2) {
+        printf("Menu:\n");
+        printf("1 - Adicionar venda\n");
+        printf("2 - Sair e exibir o total de vendas\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                if (opcao) {
+                    for ( i = 0; i < 9999; i++){
+                    printf("Nome do produto: ");
+                    scanf("%s", &nome);
+                    contador++;
+
+                    for (size_t j = 0; j < 9999; j++){
+                    printf("Preco: ");
+                    scanf("%f", &preco);
+                
+                    }
+                    for ( k = 0; k < 9999; k++) {
+                    printf("Quantidade: ");
+                    scanf("%d", &quantidade);
+                        
+                    }                  
+                                        
+                    }
+                }
+                break;
+            case 2:
+                printf("Exibindo total de vendas:\n");
+                for (int i = 0; i < vendas; i++) {
+                    printf("Produto:%s\n", nome);
+                    printf("Preço:%.2f\n", preco);
+                    printf("Quantidade:%d\n", quantidade);
+                    
+                }
+                
+               // total = preco . quantidade;
+                //printf("Total de vendas: %.2f\n", total);
+                break;
+            default:
+                printf("Opcao invalida.\n");
+        }
+    }
+
+return 0;
+}
